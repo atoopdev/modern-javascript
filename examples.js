96261da0c6b6d0b98542872d945d6664f619612f
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-02-18 12:24:47 -0800

    object restructuring

--- examples.js
+++ examples.js
@@ -1,6 +1,20 @@
+// template literals
 let word1 = 'Amanda'
 let word2 = 'Toop'
 
 // template literal
 const fullName = `${word1} ${word2}`
 document.getElementById("example").innerText = fullName
+
+// destructuring objects
+const player = {
+    name: 'Lebron James',
+    club: 'LA Lakers',
+    address: {
+        city: 'Los Angeles'
+        }
+}
+
+// below allows you to reference contents without doing player.name notation
+const {name, club, address:{city}} = player;
+console.log(`${name} plays for ${club} in ${city}`)
